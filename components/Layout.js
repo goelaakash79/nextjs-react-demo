22078d6937f785c307e7d2cf85d6ea655906dfb5
Aakash Goel <goelaakash79@gmail.com>
2020-05-05 05:31:07 +0530

    initial commit

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import Navbar from "./Navbar";
+
+const Layout = props => (
+	<div>
+		<Head>
+			<title>Demo NextJS</title>
+			<link
+				rel="stylesheet"
+				href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+				integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+				crossorigin="anonymous"
+			/>
+		</Head>
+		<Navbar />
+		<div className="container">{props.children}</div>
+	</div>
+);
+
+export default Layout;
